d8e34102527baf0f79b2606f5e8271758b8b54b5
Balavedhaa <balavedhaab@gmail.com>
2023-08-03 15:14:59 +0530

    changes

--- Assets/JSInter.js
+++ Assets/JSInter.js
@@ -1,98 +1,77 @@
 //Array
 
-const array = [10,20,"Vaishali",23.08,Date()]
-console.log("Before change :")
+//array = [10,20]
+//array.forEach(element => {
+  //  console.log(element)
+//});
+const array = [10,20,"KSP",23.08,Date()]
 for (var ele in array){
     console.log(array[ele])
 }
-console.log("After change :")
-array.push("3rd Years")
 
-for (var ele of array){
-    console.log(ele)
-}
-array.pop()
-for (var ele of array){
-    console.log(ele)
-}
-array.pop()
-array.pop()
-array.pop()
-array.pop()
-console.log("After popping all the elements")
 
-if(array.length > 0){
-    console.log("Elemetns are present")
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+console.log("After popping:")
+
+if(array.length>0){
+    console.log("elements are present")
     for(var ele of array){
         console.log(ele)
     }
 }
-//new
-const array = [10, 20, "Mayu", 23.08, Date()]
-console.log("B4 Change:")
-for (var ele in array){
-    console.log(array[ele])
+else{
+    console.log("elements are not present")
 }
-console.log("Af Change:")
-array.push("3rd Year")
-
-for (var ele of array){
-    console.log(ele)
-}
-array.pop()
-array.pop()
-console.log("AFTER POP:")
-
+array.push(1)
 if(array.length>0){
-    console.log("elements present")
-    for (var ele of array){
+    console.log("elements are present")
+    for(var ele of array){
         console.log(ele)
     }
-} else {
-    console.log("no elements")
 }
-
-// looping, for, foreach, forin, forof while, do..while
-//lets try foreach and forin
-for(i = 0; i<a;i++){
-    console.log(i)
+else{
+    console.log("elements are not present")
 }
-
-//while loop
-i = 0
-while(i <=a){
-    console.log(i)
-    i++
-}
-
-array = [10,20]
-array.forEach(element => console.log(element));
-
-//Objects
-
+//OBJECT
 a = {
-    car: "Audi", 
-    model: "A8" ,
-    airbags: 8,
-    wheels: 5,
-    color: "blue"
+    car: "Audi",
+    model: "A8",
+    airbags:8,
+    wheels:5,
+    color:"blue"
 }
 console.log(a['color'])
 console.log(a.airbags)
 var some = 'model'
 console.log(a[some])
 console.log(a)
-//change features
 a.car = "BMW"
 console.log(a)
-//add features
 a.sunroof = "Yes"
 console.log(a)
-//nested object
-a.sunroof = {isPresent: "Yes",Shape: "Rectangle"}
+
+//NESTED OBJS
+console.log(a['color'])
+console.log(a.airbags)
+var some = 'model'
+console.log(a[some])
+console.log(a)
+a.car = "BMW"
+console.log(a)
+a.sunroof = {isPresent:"YES",Shape:"Rectangle"}
 console.log(a)
 console.log(a.sunroof.isPresent)
-
-for (var ele in a){
-    console.log(ele +":" + a[ele])
+for(var ele in a){
+    console.log(ele+ ":"+ a[ele])
+    if(typeof a[ele]=="object")
+    {
+        for(var val in a[ele]){
+            console.log(val +":"+a[ele][val])
+        }
+    }
 }
