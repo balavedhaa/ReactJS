dc9e3e70798f31779424269b695e844a8664970e
Balavedhaa <balavedhaab@gmail.com>
2023-08-03 14:46:18 +0530

    changes

--- Assets/JSInter.js
+++ Assets/JSInter.js
@@ -1,5 +1,3 @@
-// Array
-
 const array = [10,20,"KSP",23.08,Date()]
 for (var ele in array){
     console.log(array[ele])
@@ -14,6 +12,16 @@ array.pop()
 array.pop()
 console.log("After popping:")
 
+if(array.length>0){
+    console.log("elements are present")
+    for(var ele of array){
+        console.log(ele)
+    }
+}
+else{
+    console.log("elements are not present")
+}
+array.push(1)
 if(array.length>0){
     console.log("elements are present")
     for(var ele of array){
