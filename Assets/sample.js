bdcd89693bbb7a091d1da0ac7777416ed8f4fa9a
Balavedhaa <balavedhaab@gmail.com>
2023-08-03 11:32:06 +0530

    changes

--- Assets/sample.js
+++ Assets/sample.js
@@ -1,18 +1,18 @@
-//var a = 5;
-//console.log("The value of this variable before altering :"+""+a);
-//{
-  //  var a =7
-    //console.log("The value of this variable after altering :"+""+a);
-
-//console.log("The value of this variable after block altering :"+""+a);
-
-let a = 9;
-console.log("The value of let before altering :"+""+a);
+var a = 5;
+console.log("The value of this variable before altering :"+""+a);
 {
-    var b =7
+    var a =7
+    console.log("The value of this variable after altering :"+""+a);
+
+console.log("The value of this variable after block altering :"+""+a);
+}
+let b= 9;
+console.log("The value of let before altering :"+""+b);
+{
+    let b =7
     console.log("The value of let after altering :"+""+b);
 }
 console.log("The value of let after block altering :"+""+b);
 
-//const c = 8;
-//console.log("The value of const before altering :"+""+c);
+const c = 8;
+console.log("The value of const before altering :"+""+c);
