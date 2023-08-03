f79425f6673a2d04adc06cbf9e6da32969388649
Balavedhaa <balavedhaab@gmail.com>
2023-08-03 16:28:30 +0530

    changes

--- Assets/JS linkforHtml.js
+++ Assets/JS linkforHtml.js
@@ -1,1 +1,16 @@
-document.write("Hello 3rd year students")
+//document.write("Hello 3rd year students")
+//alert("Dont open this page")
+//window.print("")
+
+function myFunfor1st(){
+    document.getElementById("hello").innerHTML ="I am in my 1st year of AI&DS"
+}
+function myFunfor2nd(){
+    document.getElementById("hello").innerHTML ="I am in my 2nd year of AI&DS"
+}
+function myFunfor3rd(){
+    document.getElementById("hello").innerHTML ="I am in my 3rd year of AI&DS"
+}
+function myFunfor4rth(){
+    document.getElementById("hello").innerHTML ="I am in my 4rth year of AI&DS"
+}
