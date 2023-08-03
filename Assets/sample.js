c2d0cd805330519b4386ae1b2fc1b3f04363b559
Balavedhaa <balavedhaab@gmail.com>
2023-08-03 12:15:24 +0530

    changes

--- Assets/sample.js
+++ Assets/sample.js
@@ -1,3 +1,4 @@
+//var
 var a = 5;
 console.log("The value of this variable before altering :"+""+a);
 {
@@ -6,6 +7,8 @@ console.log("The value of this variable before altering :"+""+a);
 
 console.log("The value of this variable after block altering :"+""+a);
 }
+
+//let
 let b= 9;
 console.log("The value of let before altering :"+""+b);
 {
@@ -14,5 +17,46 @@ console.log("The value of let before altering :"+""+b);
 }
 console.log("The value of let after block altering :"+""+b);
 
+//const
 const c = 8;
 console.log("The value of const before altering :"+""+c);
+
+//if..else..
+a=0
+if(a===0){
+    console.log(a)
+    console.log("The number is Zero")
+    console.log("The number is 0 and adding 1 to make it positive")
+    a=a+1
+    console.log(a)
+    console.log("The number is Positive")
+}
+
+else if(a>0){
+    console.log("The number is Positive")
+}
+else{
+    console.log("The number is Negative")
+}
+
+//looping,for,foreach,forin,forof while,do..while
+//for loop
+console.log("For loop")
+a=5
+for(i=0;i<a;i++){
+    console.log(i)
+}
+
+//while loop
+console.log("While loop")
+i=0
+while (i<=a){
+    console.log(i)
+    i++
+ }
+
+ //foreach loop
+ console.log("ForEach loop")
+ array=[10,20]
+ array.forEach(element => {console.log(element)});
+
